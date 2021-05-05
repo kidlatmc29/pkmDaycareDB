272d2e6e5920ef712e8b56bc28d532cb1217f1a9
ozabell <kidlatmc@gmail.com>
2021-05-04 18:57:30 -0700

    seeing if I can generate data for my childOf table

--- pkmDaycareDataGenerator/pkmDaycareDataGenerator.cpp
+++ pkmDaycareDataGenerator/pkmDaycareDataGenerator.cpp
@@ -5,6 +5,7 @@
 #include <iostream>
 #include <fstream>
 #include <sstream>
+#include <vector>
 
 using namespace std; 
 
@@ -13,15 +14,34 @@ const string FILE_NAME = "eggData.csv";
 int main()
 {
    string fileName;
-   cout << "What data would you like to generate:" << endl;
+
+   cout << "What data would you like to generate:";
    cin >> fileName; 
 
+   vector<string> result; 
+   stringstream ss; 
    ifstream input;
    string line;
 
    input.open(FILE_NAME);
    if (!input.fail()) {
+     while (getline(input, line)) {
+       ss.str(line);
+       while (ss.good()) {
+         string substr;
+         getline(ss, substr, ',');
+         result.push_back(substr);
+       }
+       for (int index = 0; index < result.size(); index++) {
+         cout << result[index] << " ";
+       }
 
+       ss.clear();
+     }
+ 
+   }
+   else {
+     cout << endl << "Failed to read file. Exiting program...." << endl;
    }
    
    return 0; 
